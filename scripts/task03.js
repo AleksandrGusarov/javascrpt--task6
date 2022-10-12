559dc91641a8538b4afdfc2b97c2ef9380464924
Aleksandr Gusarov <sash.gusarov@mail.ru>
2022-10-12 21:51:06 +0300

    ггзupdate

--- scripts/task03.js
+++ scripts/task03.js
@@ -1,7 +1,10 @@
 'use strict';
 
 
-const names= ["Noah", "Liam", "Mason"];
-names.forEach(elements => console.log('Mr ' + elements));
+const names= ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
+let result = names.map(function addPrefx(names) {
+    return 'Mr' + ' ' + names;
+})
+console.log(result);
 
 
